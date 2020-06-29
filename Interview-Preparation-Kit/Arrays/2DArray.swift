f39bff2a82c28985533096c0e3cbbe35573902ca
Maria Fernanda Azolin <49958403+azolinmf@users.noreply.github.com>
2020-06-29 20:15:57 -0300

    Create 2DArray.swift

---
+++ Interview-Preparation-Kit/Arrays/2DArray.swift
@@ -0,0 +1,25 @@
+//  HackerRank tests
+//  Warmup challenges
+//  2D Array
+//  Created by Maria Fernanda Azolin on 28/06/20.
+
+func hourglassSum(arr: [[Int]]) -> Int {
+
+    let initialLine = 0
+    let lastLine = 3
+    let initialColumn = 0
+    let lastColumn = 3
+
+    var sumsArray = [Int]()
+
+    for line in initialLine...lastLine {
+        for column in initialColumn...lastColumn {
+            let sum = sumOfHourGlass(arr: arr, line: line, column: column)
+            sumsArray.append(sum)
+        }
+    }
+
+    sumsArray.sort()
+
+    return sumsArray[sumsArray.count-1]
+}
