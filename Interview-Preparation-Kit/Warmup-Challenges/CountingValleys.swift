da0a31798c8b572c8c3ec9ba8f081ad49d4829a3
Maria Fernanda Azolin <49958403+azolinmf@users.noreply.github.com>
2020-06-29 19:43:22 -0300

    Create CountingValleys.swift

---
+++ Interview-Preparation-Kit/Warmup-Challenges/CountingValleys.swift
@@ -0,0 +1,26 @@
+func countingValleys(n: Int, s: String) -> Int {
+
+    var level = 0
+    var wasDescending = false
+    var numberOfValleys = 0
+
+    for direction in s {
+
+        if direction == "U" {
+            level += 1
+        } else {
+            level -= 1
+        }
+
+        if level < 0 {
+            if !wasDescending {
+                numberOfValleys += 1
+                wasDescending = true
+            }
+        } else {
+            if wasDescending {
+                wasDescending = false
+            }
+        }
+
+    }
