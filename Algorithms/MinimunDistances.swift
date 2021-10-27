ca9b91131563cc3894acec8c0cdf5df3f05fa6db
Maria Fernanda Azolin <azolin.mf@gmail.com>
2021-10-27 12:27:49 -0300

    minimun distances problem

---
+++ Algorithms/MinimunDistances.swift
@@ -0,0 +1,18 @@
+func minimumDistances(a: [Int]) -> Int {
+    var minDistance = a.count;
+    let lastPos = a.count-1
+    
+    for (index, element) in a.enumerated() {
+        if index < lastPos {
+            let aux = a[index+1...lastPos]
+            if aux.contains(element) {
+                let distance = (aux.firstIndex(of: element) ?? 0) - index
+                if distance < minDistance {
+                    minDistance = distance
+                }
+            }
+        }
+    }
+    
+    return minDistance != a.count ? minDistance : -1
+}
